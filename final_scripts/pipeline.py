36d13e2b303bbbb2302f3985244251535f1f1c94
36000 <jk23@att.net>
2019-08-19 13:34:48 -0700

    new samples!

--- final_scripts/pipeline.py
+++ final_scripts/pipeline.py
@@ -165,8 +165,8 @@ def pipeline(datasets, ischarged, usePrev = True, n = 150000):
     print(n_hyp_tbl)
 
 def main(combine = False, n = 150000):
-    datasets_c = ['h_qq_rot_charged', 'h_gg_rot_charged', 'cp_qq_rot_charged', 'qx_qg_rot_charged', 's8_gg_rot_charged', 'zp_qq_rot_charged']
-    datasets_s = ['h_qq', 'h_gg', 'cp_qq', 'qx_qg', 's8_gg', 'zp_qq']
+    datasets_c = ['h_qq_rot_charged', 'h_gg_rot_charged', 'cp_qq_rot_charged', 'qx_qg_rot_charged', 's8_gg_rot_charged', 'zp_qq_rot_charged',  'six_jj_rot_charged', 'x2_jj_rot_charged']
+    datasets_s = ['h_qq', 'h_gg', 'cp_qq', 'qx_qg', 's8_gg', 'zp_qq', 'six_jj', 'x2_jj']
 
     pipeline(datasets_s, False, n=n)
     pipeline(datasets_c, True, n=n)
